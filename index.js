15753ff451531598a6e2fe79167e6da3b64ac40e
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-05-19 17:20:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if(string === string.toLowerCase()){
     return 'I can\'t hear you!';
-  }) else if (string === string.toUpperCase()){
+  }) elseif (string === string.toUpperCase()){
     return "YES INDEED!"
-  } else if (string === "I love you, Grandma."){
-    return 
+  } elseif (string === "I love you, Grandma."){
+    return "I love you, too."
   }
 }
