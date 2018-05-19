72d9f4cfa9e4ef38c4360fd2d4a04f2de912af8f
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-05-19 17:16:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,6 @@ function sayHiToGrandma(string){
   if(string === string.toLowerCase()){
     return 'I can\'t hear you!';
   }) else if (string === string.toUpperCase()){
-    
+    return ""
   }
+}
